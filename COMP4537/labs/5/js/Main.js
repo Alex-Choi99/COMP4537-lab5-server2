fd198cf92dc4daf99bde24861b8bcd40c4316fe2
Alex Choi <sequoia.alex.ca@gmail.com>
2025-10-16 23:45:58 -0700

    Fixed the mysql detail

--- COMP4537/labs/5/js/Main.js
+++ COMP4537/labs/5/js/Main.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 const DB_CONNECTION_MSG     = "Connected to database.\n";
 const DB_HOST               = "https://phpmyadmin.co/tbl_structure.php";
-const DB_USER               = dotenv.DB_USER;
-const DB_PASSWORD           = dotenv.DB_PASSWORD;
-const DB_NAME               = dotenv.DB_NAME;
+const DB_USER               = process.env.DB_USER;
+const DB_PASSWORD           = process.env.DB_PASSWORD;
+const DB_NAME               = process.env.DB_NAME;
 const HEADER_CONTENT_TYPE   = "Content-Type";
 const HEADER_JSON_CONTENT   = "application/json";
 const GET                   = "GET";
